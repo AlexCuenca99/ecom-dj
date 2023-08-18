8a169925ed523ac332fdad9e28c252cb37279b9c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-18 18:52:38 -0500

    Add custom user manager

---
+++ applications/accounts/managers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email")
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, username, password=None, **extra_fields):
+        user = self.create_user(email, username, password=password, **extra_fields)
+        user.is_active = True
+        user.is_staff = True
+        user.is_admin = True
+        user.save()
+        return user
