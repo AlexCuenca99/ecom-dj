2c0590bf2036f5dbe055d42e6fb52eee2795165c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-18 19:23:50 -0500

    Add custom auth JWT classes

--- core/settings/local.py
+++ core/settings/local.py
@@ -68,9 +68,10 @@ SITE_NAME = "EcomDJ"
 # SimpleJWT
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
     "AUTH_HEADER_TYPES": ("Bearer",),
+    "AUTH_TOKEN_CLASSES": ("rest_framework_simplejwt.tokens.AccessToken",),
 }
