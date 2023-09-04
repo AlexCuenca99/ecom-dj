d60f55fc79c92774f1b9ac194d7af84e4105034c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-04 18:35:44 -0500

    Increase JWT lifetime

--- core/settings/local.py
+++ core/settings/local.py
@@ -67,7 +67,7 @@ SITE_NAME = "EcomDJ"
 
 # SimpleJWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=5),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
