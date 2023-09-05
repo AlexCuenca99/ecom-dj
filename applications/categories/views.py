6b0842b00b8d8b12462f524ae06438ad4a7ad54b
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-05 18:42:59 -0500

    Add ParentCategoryListAPIView request

--- applications/categories/views.py
+++ applications/categories/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
 
-# Create your views here.
+from .models import Category
+from .serializers import ParentCategorySerializer
+from .utils import parent_category_setter
+
+
+class ParentCategoryListAPIView(generics.ListAPIView):
+    queryset = Category.objects.filter(parent=None)
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = ParentCategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        data = parent_category_setter()
+        serializer = self.get_serializer(instance=data, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
