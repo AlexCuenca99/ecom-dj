91361169848baa6fed5f223460bcb9c1b6a122f8
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-05 18:43:43 -0500

    Register parent categories

--- applications/categories/urls.py
+++ applications/categories/urls.py
@@ -1,9 +1,15 @@
 from django.urls import include, path
 
 from .routers import router
+from .views import ParentCategoryListAPIView
 
 app_name = "categories_app"
 
 urlpatterns = [
     path("", include(router.urls)),
+    path(
+        "parent-categories/",
+        ParentCategoryListAPIView.as_view(),
+        name="parent-categories",
+    ),
 ]
