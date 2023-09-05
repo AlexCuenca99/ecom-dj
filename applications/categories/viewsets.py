e8465b35fe1e2e33b28ea2045d20a380d0b1c408
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-05 16:28:01 -0500

    Fix permission classes

--- applications/categories/viewsets.py
+++ applications/categories/viewsets.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .models import Category
+from .serializers import CategoryModelSerializer
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     queryset = Category
-    authentication_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = CategoryModelSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
