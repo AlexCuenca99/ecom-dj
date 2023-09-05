53af6bf7c910b22582682b573d94bcd803e05ed9
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-05 18:44:09 -0500

    Add ParentCategoryModelSerializer and ParentCategorySerializer

--- applications/categories/serializers.py
+++ applications/categories/serializers.py
@@ -8,3 +8,16 @@ class CategoryModelSerializer(serializers.ModelSerializer):
         fields = "__all__"
         read_only_fields = ["id"]
         depth = 1
+
+
+class ParentCategoryModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        read_only_fields = ["id"]
+        exclude = ["parent"]
+
+
+class ParentCategorySerializer(serializers.Serializer):
+    id = serializers.UUIDField(read_only=True)
+    name = serializers.CharField(max_length=100)
+    children = ParentCategoryModelSerializer(many=True)
