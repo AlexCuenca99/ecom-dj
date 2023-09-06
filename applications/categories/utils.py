d7b8422682a12be53dbca05fe219fa1cba363dfd
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-06 18:47:08 -0500

    Add method to retrieve only a category

--- applications/categories/utils.py
+++ applications/categories/utils.py
@@ -10,12 +10,12 @@ class ParentCategory:
     children: list
 
 
-def parent_category_setter() -> list[ParentCategory]:
+def parent_category_setter(category: Category = None) -> list[ParentCategory]:
     """Generate a query for set categories"""
 
     data = []
 
-    categories = Category.objects.filter(parent=None)
+    categories = Category.objects.filter(parent=None, id=category.pk)
 
     for category in categories:
         category_entry = ParentCategory(
@@ -24,3 +24,13 @@ def parent_category_setter() -> list[ParentCategory]:
         data.append(category_entry)
 
     return data
+
+
+def parent_category_setter_retrieve(category: Category) -> ParentCategory:
+    """Generate a query for the requested category"""
+
+    category_entry = ParentCategory(
+        id=category.id, name=category.name, children=category.category_set.all()
+    )
+
+    return category_entry
