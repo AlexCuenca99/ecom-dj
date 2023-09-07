ab5b4c681e9f11d0a1281a0693c6d72bac764b54
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-07 18:07:02 -0500

    Add filter backends

--- applications/products/viewsets.py
+++ applications/products/viewsets.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Product
 from .serializers import ProductModelSerializer
@@ -9,3 +11,13 @@ class ProductModelViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductModelSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
+    filterset_fields = {"name": ["exact"], "price": ["gte", "lte", "exact", "lt", "gt"]}
+    ordering_fields = ["price", "name"]
+    search_fields = [
+        "name",
+    ]
