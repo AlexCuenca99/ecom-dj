0c1bef8d6d4366b9218da2743039180bf4ab04a5
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-06 21:06:03 -0500

    Update read only request methods

--- applications/categories/views.py
+++ applications/categories/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
@@ -10,11 +10,11 @@ from .utils import parent_category_setter, parent_category_setter_retrieve
 
 class ParentCategoryListAPIView(generics.ListAPIView):
     queryset = Category.objects.filter(parent=None)
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     serializer_class = ParentCategorySerializer
 
-    def get(self, request, *args, **kwargs):
-        data = parent_category_setter()
+    def list(self, request, *args, **kwargs):
+        data = parent_category_setter(self.get_queryset())
         serializer = self.get_serializer(instance=data, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
@@ -22,10 +22,10 @@ class ParentCategoryListAPIView(generics.ListAPIView):
 
 class ParentCategoryRetrieveAPIView(generics.RetrieveAPIView):
     queryset = Category.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
     serializer_class = ParentCategorySerializer
 
-    def get(self, request, *args, **kwargs):
+    def retrieve(self, request, *args, **kwargs):
         category = self.get_object()
 
         data = parent_category_setter_retrieve(category)
