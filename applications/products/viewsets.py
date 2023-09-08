12e0f5f9490e5626058a12da029a16a6f805bc51
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 12:25:33 -0500

    Add related products action

--- applications/products/viewsets.py
+++ applications/products/viewsets.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, filters
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework import viewsets, filters, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from django_filters.rest_framework import DjangoFilterBackend
@@ -21,3 +23,19 @@ class ProductModelViewSet(viewsets.ModelViewSet):
     search_fields = [
         "name",
     ]
+
+    @action(
+        detail=True,
+        methods=["get"],
+        url_name="related-products",
+        url_path="related-products",
+        permission_classes=[IsAuthenticatedOrReadOnly],
+    )
+    def related_products(self, request, pk=None):
+        product = self.get_object()
+        related_products = Product.objects.exclude(id=product.id).filter(
+            category=product.category, is_available=True
+        )
+        serializer = self.get_serializer(instance=related_products, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
