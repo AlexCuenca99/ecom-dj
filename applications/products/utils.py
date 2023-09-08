053e8d7b3b57c6be5d1092600b3b6c98d1cf12db
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 11:28:51 -0500

    Remove hint type from instance

--- applications/products/utils.py
+++ applications/products/utils.py
@@ -1,12 +1,8 @@
 import os
-from datetime import datetime
-from django.conf import settings
-
-from .models import Product
 
 
 # Set article photo path
-def product_photo_file_path(instance: Product, filename) -> str:
+def product_photo_file_path(instance, filename) -> str:
     ext = filename.split(".")[-1]
     filename = "product_photo_{}.{}".format(instance.id, ext)
 
