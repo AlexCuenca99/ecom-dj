d7a217d35f182fb52d050e074156c56ef778de02
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 12:25:18 -0500

    Custom admin view

--- applications/products/admin.py
+++ applications/products/admin.py
@@ -3,4 +3,17 @@ from django.contrib import admin
 from .models import Product
 
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "compare_price", "stock", "sold")
+    list_display_links = ("id", "name")
+    list_filter = ("category",)
+    list_editable = (
+        "price",
+        "compare_price",
+        "stock",
+    )
+    search_fields = ("name",)
+    list_per_page = 25
+
+
+admin.site.register(Product, ProductAdmin)
