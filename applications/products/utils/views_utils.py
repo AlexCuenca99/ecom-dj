e5dc72187520fe5c5d9bdd1ef3bd28d41bc326d0
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-08 15:14:01 -0500

    Add get related products function

--- applications/products/utils/views_utils.py
+++ applications/products/utils/views_utils.py
@@ -1,13 +1,25 @@
 from django.db.models import QuerySet
-from applications.products.models import Product
+
+from ..models import Product
+from applications.categories.models import Category
 
 
 def get_related_products(product: Product) -> QuerySet[Product]:
-    if product.category_parent:
-        print("Producto category has parent")
-    else:
-        print("Producto category has no parent")
+    # Si el producto tiene categoria padre
+    if product.category.parent:
+        # Obtener categorias hijas
+        related_categories = product.category.parent.category_set.all()
 
-    return Product.objects.exclude(id=product.id).filter(
-        category=product.category, is_available=True
-    )
+        # Obtener productos de las categorias hijas y excluir el producto actual
+        related_products = Product.objects.filter(
+            category__in=related_categories
+        ).exclude(id=product.id)
+
+    # Si el producto no tiene categoria padre
+    else:
+        # Obtener productos de la misma categoria padre y excluir el producto actual
+        related_products = Product.objects.filter(category=product.category).exclude(
+            id=product.id
+        )
+
+    return related_products
