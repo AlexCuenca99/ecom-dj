c4c369a651af5decf2d1382a6b87fe14689cce64
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 18:06:53 -0500

    Update imports

--- applications/categories/views.py
+++ applications/categories/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics, status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 
 from .models import Category
 from .serializers import ParentCategorySerializer
