5d40088b7e6fce669a89ff94a2e9f80b1332ba49
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 16:54:53 -0500

    Define a method to filter parents (or not) categories

--- applications/products/utils/views_utils.py
+++ applications/products/utils/views_utils.py
@@ -23,3 +23,22 @@ def get_related_products(product: Product) -> QuerySet[Product]:
         )
 
     return related_products
+
+
+def get_related_products_by_category(category_id: str) -> QuerySet[Product]:
+    category = Category.objects.get(id=category_id)
+
+    # Si la categoria tiene categoria padre
+    if category.parent:
+        # Obtener productos de la misma categoria padre y excluir el producto actual
+        related_products = Product.objects.filter(category=category)
+
+    # Si la categoria no tiene categoria padre
+    else:
+        # Obtener categorias hijas
+        related_categories = category.category_set.all()
+
+        # Obtener productos de las categorias hijas
+        related_products = Product.objects.filter(category__in=related_categories)
+
+    return related_products
