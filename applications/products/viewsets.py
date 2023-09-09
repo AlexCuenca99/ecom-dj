fab7dd75e8f038de50a9732f4aad5f101fe42353
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-09 16:54:12 -0500

    Define custom query to filter categories

--- applications/products/viewsets.py
+++ applications/products/viewsets.py
@@ -2,12 +2,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import viewsets, filters, status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from uuid import UUID
 
 from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Product
 from .serializers import ProductModelSerializer
-from .utils.views_utils import get_related_products
+from .utils.views_utils import get_related_products, get_related_products_by_category
 
 
 class ProductModelViewSet(viewsets.ModelViewSet):
@@ -21,12 +22,27 @@ class ProductModelViewSet(viewsets.ModelViewSet):
     ]
     filterset_fields = {
         "price": ["gte", "lte", "exact", "lt", "gt"],
-        "category": ["exact"],
+        "category": [],
     }
     ordering_fields = ["price", "name", "created", "sold"]
     search_fields = [
         "name",
     ]
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        category_id = self.request.query_params.get("category", None)
+
+        try:
+            category_id = UUID(category_id)
+        except:
+            return super().get_queryset()
+
+        if category_id is not None:
+            queryset = get_related_products_by_category(category_id)
+            return queryset
+        else:
+            return super().get_queryset()
 
     @action(
         detail=True,
