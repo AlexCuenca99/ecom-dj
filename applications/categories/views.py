e94c630a10484e9714498635eee0b4fe8aade13a
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-11 12:11:33 -0500

    Filter by categories and return not found if any category does not exists

--- applications/categories/views.py
+++ applications/categories/views.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from rest_framework import generics, status
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
@@ -15,11 +17,37 @@ class ParentCategoryListAPIView(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ["id"]
 
-    def list(self, request, *args, **kwargs):
-        data = parent_category_setter(self.get_queryset())
-        serializer = self.get_serializer(instance=data, many=True)
+    def get_queryset(self):
+        categories_ids_params = self.request.query_params.get("id", None)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        # Comprobar que categories_ids_params se halle en la peticion
+        if categories_ids_params is not None:
+            try:
+                # Separar los ids de las categorias
+                categories_ids = categories_ids_params.split(",")
+
+                for category_id in categories_ids:
+                    category_id = UUID(category_id)
+
+                queryset = parent_category_setter(
+                    Category.objects.filter(parent=None, id__in=categories_ids)
+                )
+                return queryset
+            except:
+                return []
+        else:
+            queryset = parent_category_setter(super().get_queryset())
+            return queryset
+
+    def list(self, request, *args, **kwargs):
+        data = self.get_queryset()
+
+        if len(data) != 0:
+            serializer = self.get_serializer(instance=data, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            response = {"detail": "At least one id was not found"}
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
 
 
 class ParentCategoryRetrieveAPIView(generics.RetrieveAPIView):
