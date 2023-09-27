60f51beab5da79ff34532389f8f22de943c969c1
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-27 11:59:14 -0500

    fix: add permissions to SAFE METHODS

--- applications/cart/viewsets.py
+++ applications/cart/viewsets.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, SAFE_METHODS
+
 from .models import Cart
 from .serializers import CartModelSerializer
 
@@ -7,4 +8,9 @@ from .serializers import CartModelSerializer
 class CartModelViewSet(ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartModelSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_permissions(self):
+        if self.action in SAFE_METHODS:
+            return [IsAuthenticated()]
+        else:
+            return [IsAdminUser()]
