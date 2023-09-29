562bbf0df5a87dd1f4f97cca02d0251e1e2f997c
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-09-28 20:38:09 -0500

    feature: serialize the cart with its items

--- applications/cart/serializers.py
+++ applications/cart/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
-from .models import Cart
+from .models import Cart, CartItem
+from applications.products.serializers import ProductModelSerializer
+
+
+class CartItemModelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        exclude = [
+            "cart",
+        ]
 
 
 class CartModelSerializer(serializers.ModelSerializer):
+    products = CartItemModelSerializer(source="items", many=True)
+
     class Meta:
         model = Cart
         fields = "__all__"
