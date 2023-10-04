4bc890089cd30ae1779ea02975feaeaf5cca57ee
Alex Cuenca <alex-patricio1999@hotmail.com>
2023-10-04 13:02:41 -0500

    feature: register cart items urls

--- applications/cart/routers.py
+++ applications/cart/routers.py
@@ -1,7 +1,8 @@
 from rest_framework import routers
 
-from .viewsets import CartModelViewSet
+from .viewsets import CartModelViewSet, CartItemModelViewSet
 
 router = routers.DefaultRouter()
 
+router.register(r"carts/<str:pk>/items", CartItemModelViewSet, basename="carts-items")
 router.register(r"carts", CartModelViewSet, basename="carts")
