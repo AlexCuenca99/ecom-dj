ed460acc776f0f9e91f489dabd16da1ddd3250d5
Alex Cuenca <alex-patricio1999@hotmail.com>
2023-10-04 13:04:11 -0500

    fix: make cart and cart item serializers independent

--- applications/cart/serializers.py
+++ applications/cart/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 
 from .models import Cart, CartItem
 from applications.products.models import Product
-from applications.products.serializers import ProductModelSerializer
 
 
 class CartItemModelSerializer(serializers.ModelSerializer):
@@ -20,29 +19,37 @@ class CartModelSerializer(serializers.ModelSerializer):
         model = Cart
         fields = "__all__"
 
-    def update(self, instance: Cart, validated_data):
-        cart_items = validated_data.pop("items")
 
-        for cart_item in cart_items:
-            cart_item_product_instance: Product = cart_item.get("product")
-            cart_item_quantity = cart_item.get("quantity")
+# class CartItemModelSerializer(serializers.ModelSerializer):
+# products = CartItemModelSerializer(source="items", many=True)
 
-            # Validate if the product stock is greater than the quantity of the
-            # cart item
-            if cart_item_quantity > cart_item_product_instance.stock:
-                raise serializers.ValidationError(
-                    {
-                        "message": f"Stock is not enough for {cart_item_product_instance.name}"
-                    },
-                    code="not-enough-product-stock",
-                )
+# class Meta:
+# model = CartItem
+# fields = "__all__"
 
-            if cart_item_product_instance in instance.products.all():
-                raise serializers.ValidationError(
-                    {
-                        "message": f"{cart_item_product_instance.name} is already in cart."
-                    },
-                    code="product-already-in-cart",
-                )
+# def update(self, instance: Cart, validated_data):
+# cart_items = validated_data.pop("items")
 
-        return True
+# for cart_item in cart_items:
+# cart_item_product_instance: Product = cart_item.get("product")
+# cart_item_quantity = cart_item.get("quantity")
+
+# # Validate if the product stock is greater than the quantity of the
+# # cart item
+# if cart_item_quantity > cart_item_product_instance.stock:
+# raise serializers.ValidationError(
+# {
+# # "message": f"Stock is not enough for {cart_item_product_instance.name}"
+# },
+# code="not-enough-product-stock",
+# )
+
+# if cart_item_product_instance in instance.products.all():
+# raise serializers.ValidationError(
+# {
+# # "message": f"{cart_item_product_instance.name} is already in cart."
+# },
+# code="product-already-in-cart",
+# )
+
+# return True
