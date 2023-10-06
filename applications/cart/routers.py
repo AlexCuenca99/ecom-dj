05665bff35820a494ecc6980ba1d6b1189a07fcd
Alex Cuenca <alex-patricio1999@hotmail.com>
2023-10-05 23:28:59 -0500

    feature: add cart item serializers

--- applications/cart/routers.py
+++ applications/cart/routers.py
@@ -4,5 +4,7 @@ from .viewsets import CartModelViewSet, CartItemModelViewSet
 
 router = routers.DefaultRouter()
 
-router.register(r"carts/<str:pk>/items", CartItemModelViewSet, basename="carts-items")
 router.register(r"carts", CartModelViewSet, basename="carts")
+router.register(
+    r"carts/(?P<id>[^/.]+)/items", CartItemModelViewSet, basename="carts-items"
+)
